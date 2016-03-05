710cf5116d40bb742711d87e7f80675d64e9cf4b
Philip Yoo <philipyoo10@gmail.com>
2016-03-05 01:37:00 -0800

    Edit view page for rest of email layout, add some styling

--- src/components/email/email.ts
+++ src/components/email/email.ts
@@ -1,26 +1,24 @@
 import {Component, Input} from 'angular2/core';
 import {AddContactComponent} from './add-contact';
 import {AddHiddenContactComponent} from './add-hidden-contact';
-import {ToListService} from './to-list.service'
+import {ToListService} from './to-list.service';
 
 @Component({
     selector: 'email',
     template: require('./email.html'),
-    inputs: ['contacts'],
+    inputs: ['contacts', 'from'],
     directives: [AddContactComponent, AddHiddenContactComponent],
     providers: [ToListService]
 })
 
 export class EmailComponent {
     @Input() inputChange;
-    emptyIt: boolean;
 
     constructor(private _toListService: ToListService) {}
 
     addSelectedContact(contact: any) {
         this._toListService.addContact(contact);
     }
-
 
 }
 
