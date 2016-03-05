7027d6fdab910062b7595e01862436325c3a8177
Philip Yoo <philipyoo10@gmail.com>
2016-03-04 16:36:52 -0800

    Update root component

--- src/routes/app-shell/app-shell.ts
+++ src/routes/app-shell/app-shell.ts
@@ -1,11 +1,24 @@
-import {Component} from "angular2/core";
-import {CardComponent} from "../../components/card/card";
+import {Component, OnInit} from "angular2/core";
+import {EmailComponent} from '../../components/email/email';
+import {ContactDetailComponent} from '../../components/contact/contact-detail';
+import {Contact} from '../../components/contact/contact';
+import {ContactService} from '../../components/contact/contact.service';
 
 @Component({
     selector: 'app-shell',
     template: require('./app-shell.html'),
-    directives: [CardComponent]
+    directives: [EmailComponent, ContactDetailComponent],
+    providers: [ContactService]
 })
-export class AppShell {
+export class AppShell implements OnInit {
+    contacts: Contact[];
+    constructor(private _contactService: ContactService) { }
 
+    getContacts() {
+        this._contactService.getContacts().then(contacts => this.contacts = contacts);
+    }
+
+    ngOnInit() {
+        this.getContacts();
+    }
 }
